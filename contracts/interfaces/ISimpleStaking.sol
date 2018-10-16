f41fc31c0d80bf2d5069adc34cb8de362ee1cca8
Hoang Nguyen <hoang@wetrust.io>
2018-10-16 14:42:03 -0700

    use interface instead of contract. Hence, methods are external

--- contracts/interfaces/ISimpleStaking.sol
+++ contracts/interfaces/ISimpleStaking.sol
@@ -1,18 +1,19 @@
 pragma solidity ^0.4.22;
 
 /// https://eips.ethereum.org/EIPS/eip-900
+/// @notice Interface with external methods
 interface ISimpleStaking {
 
   event Staked(address indexed user, uint256 amount, uint256 total, bytes data);
   event Unstaked(address indexed user, uint256 amount, uint256 total, bytes data);
 
-  function stake(uint256 amount, bytes data) public;
-  function stakeFor(address user, uint256 amount, bytes data) public;
-  function unstake(uint256 amount, bytes data) public;
-  function totalStakedFor(address addr) public view returns (uint256);
-  function totalStaked() public view returns (uint256);
-  function token() public view returns (address);
-  function supportsHistory() public pure returns (bool);
+  function stake(uint256 amount, bytes data) external;
+  function stakeFor(address user, uint256 amount, bytes data) external;
+  function unstake(uint256 amount, bytes data) external;
+  function totalStakedFor(address addr) external view returns (uint256);
+  function totalStaked() external view returns (uint256);
+  function token() external view returns (address);
+  function supportsHistory() external pure returns (bool);
 
   // optional. Commented out until we have valid reason to implement these methods
   // function lastStakedFor(address addr) public view returns (uint256);
--- contracts/interfaces/IVotingStandard.sol
+++ contracts/interfaces/IVotingStandard.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.22;
 
 /**
  * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1202.md
+ * @notice Use interface instead of contract
  * - Multiple issue
  * - Multiple selection
  * - Ordered multiple result
@@ -14,17 +15,17 @@ pragma solidity ^0.4.22;
 interface IVotingStandard {
 
     // Vote with an option. The caller needs to handle success or not
-  function vote(uint issueId, uint option) public returns (bool success);
-  function setStatus(uint issueId, bool isOpen) public returns (bool success);
+  function vote(uint issueId, uint option) external returns (bool success);
+  function setStatus(uint issueId, bool isOpen) external returns (bool success);
 
-  function issueDescription(uint issueId) public view returns (string desc);
-  function availableOptions(uint issueId) public view returns (uint[] options);
-  function optionDescription(uint issueId, uint option) public view returns (string desc);
-  function ballotOf(uint issueId, address addr) public view returns (uint option);
-  function weightOf(uint issueId, address addr) public view returns (uint weight);
-  function getStatus(uint issueId) public view returns (bool isOpen);
-  function weightedVoteCountsOf(uint issueId, uint option) public view returns (uint count);
-  function topOptions(uint issueId, uint limit) public view returns (uint[] topOptions_);
+  function issueDescription(uint issueId) external view returns (string desc);
+  function availableOptions(uint issueId) external view returns (uint[] options);
+  function optionDescription(uint issueId, uint option) external view returns (string desc);
+  function ballotOf(uint issueId, address addr) external view returns (uint option);
+  function weightOf(uint issueId, address addr) external view returns (uint weight);
+  function getStatus(uint issueId) external view returns (bool isOpen);
+  function weightedVoteCountsOf(uint issueId, uint option) external view returns (uint count);
+  function topOptions(uint issueId, uint limit) external view returns (uint[] topOptions_);
 
   event OnVote(uint issueId, address indexed _from, uint _value);
   event OnStatusChange(uint issueId, bool newIsOpen);
