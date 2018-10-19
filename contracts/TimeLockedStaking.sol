40dfd06a0219df9d090b66a4b9d3789daecfe321
Hoang Nguyen <hoang@wetrust.io>
2018-10-18 22:56:50 -0700

    finish time-locked stake. Pending testing

--- contracts/TimeLockedStaking.sol
+++ contracts/TimeLockedStaking.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.25;
 import "./interfaces/ERC165.sol";
 import "./interfaces/ISimpleStaking.sol";
 import "./lib/SafeMath.sol";
+import "./lib/ERC20.sol";
 
 
 contract TimeLockedStaking is ERC165, ISimpleStaking {
@@ -15,15 +16,20 @@ contract TimeLockedStaking is ERC165, ISimpleStaking {
   }
 
   /// @dev Address of the ERC20 token contract used for staking
-  address internal erc20Token;
+  ERC20 internal erc20Token;
 
   /// @dev https://solidity.readthedocs.io/en/v0.4.25/style-guide.html#avoiding-naming-collisions
   uint256 internal totalStaked_ = 0;
 
   mapping (address => StakeInfo) public stakers;
 
+  modifier greaterThanZero(uint256 num) {
+    require(num > 0, "Must be greater than 0.");
+    _;
+  }
+
   constructor(address token) public {
-    erc20Token = token;
+    erc20Token = ERC20(token);
   }
 
   /// @dev Implement ERC165
@@ -39,26 +45,33 @@ contract TimeLockedStaking is ERC165, ISimpleStaking {
     this.stakeFor(msg.sender, amount, data);
   }
 
-  function stakeFor(address user, uint256 amount, bytes data) external {
-    // TODO transfer ERC20
-    // TODO calculate effectiveAt and unlockedAt
-    if (stakers[user].amount > 0) {
-      stakers[user].amount = stakers[user].amount.add(amount);
-    } else {
-      stakers[user] = StakeInfo(amount, block.timestamp, block.timestamp);
-    }
+  function stakeFor(address user, uint256 amount, bytes data) external greaterThanZero(amount) {
+    require(erc20Token.transferFrom(msg.sender, address(this), amount));
+
+    StakeInfo memory info = stakers[user];
+
+    uint256 effectiveAt = info.effectiveAt == 0 ? block.timestamp : info.effectiveAt;
+    uint256 unlockedAt = max(info.unlockedAt, getUnlockedAtSignal(data));
+
+    stakers[user] = StakeInfo(amount.add(stakers[user].amount), effectiveAt, unlockedAt);
 
     totalStaked_ = totalStaked_.add(amount);
     emit Staked(user, amount, stakers[user].amount, data);
   }
 
-  function unstake(uint256 amount, bytes data) external {
-    // TODO return ERC20
-    // Reset effectiveAt
+  function unstake(uint256 amount, bytes data)
+    external
+    greaterThanZero(stakers[msg.sender].effectiveAt) // must be a member
+    greaterThanZero(block.timestamp - stakers[msg.sender].unlockedAt) // must be unlocked
+    greaterThanZero(amount)
+  {
     address user = msg.sender;
     stakers[user].amount = stakers[user].amount.sub(amount);
+    stakers[user].effectiveAt = block.timestamp;
 
     totalStaked_ = totalStaked_.sub(amount);
+
+    require(erc20Token.transfer(user, amount));
     emit Unstaked(user, amount, stakers[user].amount, data);
   }
 
@@ -71,10 +84,35 @@ contract TimeLockedStaking is ERC165, ISimpleStaking {
   }
 
   function token() external view returns (address) {
-    return erc20Token;
+    return address(erc20Token);
   }
 
   function supportsHistory() external pure returns (bool) {
     return false;
+  }
+
+  /// Helpers
+  ///
+
+  function max(uint256 a, uint256 b) public pure returns (uint256) {
+    return a > b ? a : b;
+  }
+
+  function min(uint256 a, uint256 b) public pure returns (uint256) {
+    return a > b ? b : a;
+  }
+
+  function getUnlockedAtSignal(bytes data) public view returns (uint256) {
+    bool signalFlag; // equivalent to uint8
+    uint256 unlockedAt;
+    assembly {
+      signalFlag := mload(add(data, 8))
+      unlockedAt := mload(add(data, 264))
+    }
+
+    // Maximum 365 days from now
+    uint256 oneYearFromNow = block.timestamp + 365 days;
+
+    return signalFlag ? min(unlockedAt, oneYearFromNow) : 0;
   }
 }
