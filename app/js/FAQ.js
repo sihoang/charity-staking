6d140e55f297b48c8cf7f06a5cdcd0a6c33f3f83
Hoang Nguyen <hoang@wetrust.io>
2018-10-26 14:51:25 -0700

    template for FAQ items

--- app/js/FAQ.js
+++ app/js/FAQ.js
@@ -3,19 +3,38 @@ import classnames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import Section from './Section';
 import SectionHeader from './SectionHeader';
+import FAQItem from './FAQItem';
+import faq from './faq.json';
 
-const styles = {
+const styles = theme => ({
   root: {
-    minHeight: '70vh',
   },
-};
+  container: {
+    paddingTop: theme.mixins.toolbar.minHeight,
+    margin: 'auto',
+    maxWidth: theme.breakpoints.values.md,
+  },
+});
 
 class FAQ extends React.Component {
+  renderFAQItems() {
+    return faq.map(item => (
+      <FAQItem
+        key={item.question}
+        question={item.question}
+        answer={item.answer}
+      />
+    ));
+  }
+
   render() {
     const { classes, className } = this.props;
     return (
       <Section primary className={classnames(classes.root, className)}>
         <SectionHeader>FAQ</SectionHeader>
+        <div className={classes.container}>
+          {this.renderFAQItems()}
+        </div>
       </Section>
     );
   }
