db4d04fdc8910ab86f12093a2985cfaa88d1269f
Hoang Nguyen <hoang@wetrust.io>
2018-11-27 02:44:14 -0800

    tolerate up to 5 minutes when compare time unlocked

--- test/stakingTimeLocked.js
+++ test/stakingTimeLocked.js
@@ -9,6 +9,11 @@ const StakingContract = embark.require('Embark/contracts/TimeLockedStaking');
 
 testSetup(() => {});
 
+// block.timestamp can vary
+// the whole suite might take up to 5 minutes
+// the unlocked time calculated by blockchain
+// should be under 5 minutes difference
+const TOLERANCE_IN_SECS = 60 * 5;
 
 // Matrix:
 // [
@@ -20,11 +25,15 @@ const runMatrix = (matrix) => {
     it(testCase[0], async () => {
       const input = buildBytesInput(testCase[1], testCase[2]);
       const timeLocked = await StakingContract.methods.getUnlockedAtSignal(input).call();
-      assert.equal(timeLocked, testCase[3]);
+      assert.ok(
+        Math.abs(timeLocked - testCase[3]) < TOLERANCE_IN_SECS,
+        `Expected ${testCase[3]} vs. Actual ${timeLocked}`,
+      );
     });
   }
 };
 
+// all in seconds
 const now = Math.floor(Date.now() / 1000);
 const oneDay = 24 * 60 * 60;
 const tomorrow = now + oneDay;
@@ -74,9 +83,10 @@ contract('Test getUnlockedAtSignal edge cases', () => {
   it('should return 1 year max', async () => {
     const input = buildBytesInput(moreThanAYearFromNow, '0');
     const timeLocked = await StakingContract.methods.getUnlockedAtSignal(input).call();
-    // compare by the minutes instead of seconds
-    // because block.timestamp can vary
-    assert.equal(Math.floor(timeLocked / 60), Math.floor(oneYearFromNow / 60));
+    assert.ok(
+      Math.abs(timeLocked - oneYearFromNow) < TOLERANCE_IN_SECS,
+      `Expected ${oneYearFromNow} vs. Actual ${timeLocked}`,
+    );
   });
 });
 
