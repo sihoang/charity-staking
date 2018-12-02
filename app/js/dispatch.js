4499a54ace586197e36c935026c0609a60afb00d
Hoang Nguyen <hoang@wetrust.io>
2018-12-01 22:33:40 -0800

    handle bad invalid ein from payload

--- app/js/dispatch.js
+++ app/js/dispatch.js
@@ -2,7 +2,7 @@
 // Some dispatch functions require multiple steps
 // in preparation
 
-
+import assert from 'assert';
 import axios from 'axios';
 import loadContract, { parseStakePayload } from './loadContract';
 import {
@@ -13,6 +13,7 @@ import {
 const { BN } = web3.utils;
 
 export const dispatchAccountActivities = (store, account) => {
+  // Get all the Staked events related to the current account
   loadContract('TimeLockedStaking').getPastEvents('Staked', {
     fromBlock: 0,
     toBlock: 'latest',
@@ -20,6 +21,7 @@ export const dispatchAccountActivities = (store, account) => {
       user: account,
     },
   }, (err, events) => {
+    // Massage the results
     const transformed = events.map((event) => {
       const {
         id, blockNumber, transactionHash, returnValues,
@@ -36,15 +38,23 @@ export const dispatchAccountActivities = (store, account) => {
       };
     });
 
+    // Call CMS to get NPO details
     const populatedNPOPromises = transformed.map(async (record) => {
-      const res = await axios.get(
-        `${CMS_URL}/charities?search=${record.ein}`,
-      );
-      const npo = res.data && res.data.records && res.data.records[0];
-      return {
-        ...npo,
-        ...record,
-      };
+      let res;
+      try {
+        // Users can pass in any EIN when they stake.
+        // If ein is invalid or not found,
+        // just show default name 'Not Found'
+        assert(record.ein.length > 0, 'Invalid EIN.');
+        res = await axios.get(
+          `${CMS_URL}/charities?search=${record.ein}`,
+        );
+      } catch (e) {
+        console.log(e);
+      }
+
+      const npo = res && res.data && res.data.records && res.data.records[0];
+      return Object.assign({ name: 'Not Found' }, npo, record);
     });
 
     Promise.all(populatedNPOPromises).then((completed) => {
